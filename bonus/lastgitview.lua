7323f220a35b99c561832d969dd02cf101dab5ca
Golem3251 <56472916+SmallGolem@users.noreply.github.com>
2023-03-15 15:47:54 +0100

    update bonus

--- bonus/lastgitview.lua
+++ bonus/lastgitview.lua
@@ -1,5 +1,6 @@
 local json = require("json")
 local timestamp = require("timestamp")
+local middleTime = 604800
 local monitor = peripheral.find("monitor")
 local token = "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"
 
@@ -9,11 +10,11 @@ while true do
   local request = http.get("https://worldtimeapi.org/api/timezone/Etc/UTC")
   currentTime = json.decode(request.readAll()).unixtime
   width, height = term.getSize()
-  size = 1/((currentTime-time)*604800)*(width-6)
+  size = 1/((currentTime-time+middleTime)/(2*middleTime))*(width-6)
   term.setBackgroundColor(colors.black)
   term.clear()
   paintutils.drawBox(2, 3, width-2, height/3*2, colors.white)
-  paintutils.drawFilledBox(3, 4, width-size-3, height/3*2-1, colors.yellow)
+  paintutils.drawFilledBox(3, 4,3+size, height/3*2-1, colors.yellow)
   term.setCursorPos(width/2-#tostring(currentTime-time)/2,height/3*2+2)
   term.setBackgroundColor(colors.black)
   --term.write(tostring(currentTime-time))
